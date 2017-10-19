8d90635059ea5beee9bc2d4ab78d19114478fa95
Jake Engel <jakeengel01@gmail.com>
2017-10-19 17:33:43 +0200

    This challenge blew. I hate sinatra. There are literally no helpful online tutorials.

--- lib/app.rb
+++ lib/app.rb
@@ -6,8 +6,23 @@ DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/jukebox.sqlite'
 
 get "/" do
   # TODO: Gather all artists to be displayed on home page
+  @artists = DB.execute(%(SELECT ar.name FROM artists ar)).flatten!.uniq.sort
+  #@artists.each { |artist| params[:artist]}
   erb :home # Will render views/home.erb file (embedded in layout.erb)
 end
+
+get "/:name" do
+  @name = params[:name]
+  @albums = DB.execute(%(SELECT al.title FROM albums al)).flatten!.uniq.sort
+  erb :artists
+end
+
+get "/:name/:album" do
+  @album = params[:album]
+  @tracks = DB.execute(%(SELECT tr.name FROM tracks tr)).flatten!.uniq.sort
+  erb :tracks
+end
+
 
 # Then:
 # 1. Create an artist page with all the albums. Display genres as well
